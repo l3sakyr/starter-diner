d14aed0a7cb8d3751a7ef0de89feccc58040f999
ck <ck@CK>
2016-11-10 17:37:01 -0800

    4. Finished Improve Form Appearance 5. Finished Use the Proper Field Types 6. Finished Wire the Form to Our Maintenance Controller

--- application/controllers/CRUD.php
+++ application/controllers/CRUD.php
@@ -6,20 +6,20 @@ class Crud extends Application
 		$this->data['pagebody'] = 'mtce';//added//
 	
 		$result = '';
-        $this->data['userrole'] = $this->session->userdata('userrole');
-        if ($this->data['userrole'] == NULL) {
-            $this->data['userrole'] = '?';
-        }
-        else if($this->data['userrole'] == 'admin') {
-        	$result = 'Admin entered';
-			$this->data['items'] = $this->menu->all();//added//
-        }
-        else if($this->data['userrole'] == 'user') {
-        	$result = 'Admin only no user';
-			$this->data['content'] = $result;
-        }
-        
-	    $this->render();
+                $this->data['userrole'] = $this->session->userdata('userrole');
+                if ($this->data['userrole'] == NULL) {
+                    $this->data['userrole'] = '?';
+                }
+                else if($this->data['userrole'] == 'admin') {
+                        $result = 'Admin entered';
+                                $this->data['items'] = $this->menu->all();//added//
+                }
+                else if($this->data['userrole'] == 'user') {
+                        $result = 'Admin only no user';
+                                $this->data['content'] = $result;
+                }
+
+                    $this->render();
 	}
 	public function edit($id=null) {
 		//session
@@ -34,14 +34,20 @@ class Crud extends Application
 			$this->session->set_userdata('record',$record);
 		}
 		
-		$this->data['content'] = "Looking at " . $key . ':' . $record->name;
+		//$this->data['content'] = "Looking at " . $key . ':' . $record->name;
 		$this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
 		$this->data['fname'] = makeTextField('Item name', 'name', $record->name);
 		$this->data['fdescription'] = makeTextField('Description', 'description', $record->description);
+                $this->data['fdescription'] = makeTextArea('Description', 'description', $record->description);
 		$this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
 		$this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
 		$this->data['fcategory'] = makeTextField('Category', 'category', $record->category);
-		
+		$cats = $this->categories->all(); // get an array of category objects
+                foreach($cats as $code => $category) // make it into an associative array
+                $codes[$code] = $category->name;
+                $this->data['fcategory'] = makeCombobox('Category', 'category', $record->category,$codes);
+                $this->data['zsubmit'] = makeSubmitButton('Save', 'Submit changes');
+                
 		$this->data['pagebody'] = "mtce-edit";
 		$this->render();
 	}
